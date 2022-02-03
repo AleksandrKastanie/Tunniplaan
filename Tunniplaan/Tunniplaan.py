b565a3e02159b93db5de9aa1268fb86a01163516
opilane <opilane@HP-CZC6297BB1>
2022-02-03 12:37:06 +0200

    12312

--- Tunniplaan/Tunniplaan.py
+++ Tunniplaan/Tunniplaan.py
@@ -1,5 +1,7 @@
 from tkinter import*
 from tkinter.messagebox import*
+import time
+import os
 def failist_sõnastikusse():
 	tund_kirjeldus={}
 	file=open("Tund.txt","r")
@@ -13,15 +15,14 @@ def failist_sõnastikusse():
 def kirjeldus_aknasse(t:str):
 	if(askyesno("Küsimus", "Kas tahad kirjeldust näha?")):
 		alam_aken=Toplevel()
-		alam_aken.title(t)
+		alam_aken.geometry("400x400")
 		lbl_kirjeldus=Label(alam_aken,text=tund_kirjeldus[t]).pack()
 		c=Canvas(alam_aken,height=500, width=500)
-		file=PhotoImage(file="")
-		c.create_image(10,10)
+		file=PhotoImage(file=t)
+		c.create_image(10,10, image=file,anchor=NW)
 		alam_aken.mainloop
 
-#frameCnt = 7
-#frames = [PhotoImage(files="название гифки.gif", format = "gif -index %i" %(i)) for i]
+
 tund_kirjeldus=failist_sõnastikusse()
 
 
@@ -90,5 +91,22 @@ Label(tunniplaan,text =" ",font="Arial 22",height=3,width=6, relief="ridge").gri
 Label(tunniplaan,text =" ",font="Arial 22",height=3,width=6, relief="ridge").grid(row=9,column=5, rowspan=2, sticky=N+S+W+E)
 Label(tunniplaan,text =" ",font="Arial 22",height=3,width=6, relief="ridge").grid(row=0,column=0, sticky=N+S+W+E)
 
+i1=PhotoImage(file="")
+r1=Label(tunniplaan,image=i1)
+r1.place(x=160,y=480)
+frameCnt = 44
+frames = [PhotoImage(file='mult.gif',format = 'gif -index %i' %(i)) for i in range(frameCnt)]
+
+def update(ind):
+
+    frame = frames[ind]
+    ind += 1
+    if ind == frameCnt:
+        ind = 0
+    label.configure(image=frame)
+    tunniplaan.after(100, update, ind)
+label = Label(aken)
+label.place(x=500,y=500)
+aken.after(0, update, 0)
 
 tunniplaan.mainloop()
